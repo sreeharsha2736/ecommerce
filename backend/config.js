9fe7b746aae8641374adb8a5607f7e99d607e1bb
sreeharsha2736 <73603062+sreeharsha2736@users.noreply.github.com>
2022-04-07 02:48:22 +0530

    mongodb

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,6 @@ export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL:
     process.env.MONGODB_URL ||
-    "mongodb+srv://sreeharsha:mongodb@cluster0.aiglg.mongodb.net/sample?retryWrites=true&w=majority",
+    "mongodb+srv://sreeharsha:mongodb@cluster0.aiglg.mongodb.net/test",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
 };
