00e46b65a9e613e2fbcbe524924b10ab6e62e53f
sreeharsha2736 <73603062+sreeharsha2736@users.noreply.github.com>
2022-04-06 23:56:33 +0530

    updated-new-last

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import products from "./data.js";
 import dotenv from "dotenv";
+import path from "path";
 import config from "./config.js";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import userRoute from "./routes/userRoute.js";
 dotenv.config();
+const __dirname = path.resolve();
 
+var app = express();
 const mongodbUrl = config.MONGODB_URL;
 mongoose
   .connect(mongodbUrl, {
@@ -16,7 +19,6 @@ mongoose
   })
   .catch((error) => console.log(error.reason));
 
-const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.get("/products", (req, res) => {
@@ -26,4 +28,12 @@ app.get("/products/:id", (req, res) => {
   const productId = req.params.id;
   res.send(products.find((x) => x.id == productId));
 });
-app.listen(5000, () => console.log("sever started"));
+
+app.use(express.static(path.join(__dirname, "/./frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/./frontend/build/index.html`));
+});
+
+app.listen(config.PORT, () => {
+  console.log("Server started at http://localhost:8080");
+});
